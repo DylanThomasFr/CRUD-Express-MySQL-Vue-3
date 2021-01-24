f8034a70af7472f75de36d4ef5ae38a3ab0a363a
Dylan THOMAS <thomas.dylan191@gmail.com>
2021-01-24 16:36:27 +0100

    Add Posts

---
+++ backend/migrations/20210124153212-create-post.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, DataTypes) => {
+    await queryInterface.createTable('posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      title: {
+        type: DataTypes.STRING
+      },
+      content: {
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    });
+  },
+  down: async (queryInterface, DataTypes) => {
+    await queryInterface.dropTable('posts');
+  }
+};
